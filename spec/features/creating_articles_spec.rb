7f4af3e4943c1a71353ccf7474f8120f1575205f
John Daugherty <johndaugherty1986@gmail.com>
2017-07-14 21:35:18 -0500

    added article validation

--- spec/features/creating_articles_spec.rb
+++ spec/features/creating_articles_spec.rb
@@ -14,6 +14,7 @@ RSpec.feature "Creating Articles" do
     expect(page).to have_content("Article has been created")
     expect(page.current_path).to eq(articles_path)
   end
+
   scenario "A user fails to create a new article" do
     visit "/"
 
@@ -23,8 +24,9 @@ RSpec.feature "Creating Articles" do
     fill_in "Body", with: ""
 
     click_button "Create Article"
-    
+
     expect(page).to have_content("Article has not been created")
-    expect(page).to have_content("Title can't be blank") expect(page).to have_content("Body can't be blank")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
   end
 end
